8edded92ff0cca63626e3de174454972bc61d899
Mike Quinlan <mquinlan@mines.edu>
2012-01-18 22:51:58 -0700

    going to level 6

--- project02/rubywarrior/rubyus-maximus-beginner/player.rb
+++ project02/rubywarrior/rubyus-maximus-beginner/player.rb
@@ -7,7 +7,11 @@ class Player
 		if warrior.feel.empty?
 			warrior.walk!
 		else
-			warrior.attack!
+			if warrior.feel.captive?
+				warrior.rescue!
+			else
+				warrior.attack!
+			end
 		end
 	end
 	@old_hp = warrior.health

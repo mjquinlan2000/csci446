ce58a89d7f4d0d0a08127e997675aae3a5a858c6
Mike Quinlan <mquinlan@mines.edu>
2012-01-19 22:59:46 -0700

    at level 7

--- project02/rubywarrior/rubyus-maximus-beginner/player.rb
+++ project02/rubywarrior/rubyus-maximus-beginner/player.rb
@@ -1,16 +1,44 @@
 class Player
 	@old_hp = 20
+	@back = false
+	@resting_at_wall = false
   def play_turn(warrior)
-	if warrior.health < 20 and warrior.feel.empty? and @old_hp <= warrior.health
+	if @resting_at_wall == true and warrior.health < 20
 		warrior.rest!
 	else
-		if warrior.feel.empty?
+		if @resting_at_wall == true and warrior.health == 20
+			@resting_at_wall = false
 			warrior.walk!
 		else
-			if warrior.feel.captive?
-				warrior.rescue!
+			if @back == true
+				if warrior.feel(:backward).empty?
+					warrior.walk!(:backward)
+				else
+					if warrior.feel(:backward).captive?
+						warrior.rescue!(:backward)
+					else
+						if warrior.feel(:backward).wall?
+							@back = false
+							@resting_at_wall = true
+							warrior.rest!
+						end
+					end
+				end
 			else
-				warrior.attack!
+				if warrior.health < 10 and @old_hp > warrior.health
+					@back = true
+					warrior.walk!(:backward)
+				else
+					if warrior.feel.empty? and @old_hp == warrior.health and warrior.health < 20
+						warrior.rest!
+					else
+						if warrior.feel.empty?
+							warrior.walk!
+						else
+							warrior.attack!
+						end
+					end
+				end
 			end
 		end
 	end

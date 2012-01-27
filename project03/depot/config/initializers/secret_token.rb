e52729f35c706a3634a211ed3c6a80a3deda0006
Mike Quinlan <mquinlan@mines.edu>
2012-01-27 10:05:24 -0700

    removed depot project moving back to rails -v 3.1.3

--- project03/depot/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Depot::Application.config.secret_token = 'add117e6f311ca579b801f100ab6a181479977a54351133e6a99ec32741a15a39d6bd94543c5ca3748f818fa9d8922bfb594e7c5d66188c9f184b852617b1f58'

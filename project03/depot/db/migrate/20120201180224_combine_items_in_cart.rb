0b9ae9bcdecf84d3bcecb56f1b5cb0667edf080b
Mike Quinlan <mquinlan@mines.edu>
2012-02-01 11:18:18 -0700

    added migration to combine line items in the cart

---
+++ project03/depot/db/migrate/20120201180224_combine_items_in_cart.rb
@@ -0,0 +1,16 @@
+class CombineItemsInCart < ActiveRecord::Migration
+  def up
+    Cart.all.each do |cart|
+      sums = cart.line_items.group(:product_id).sum(:quantity)
+      sums.each do |product_id, quantity|
+	if quantity > 1
+	  cart.line_items.where(:product_id => product_id, :quantity => quantity).delete_all
+	  cart.line_items.create(:product_id => product_id, :quantity => quantity)
+	end
+      end
+    end
+  end
+
+  def down
+  end
+end

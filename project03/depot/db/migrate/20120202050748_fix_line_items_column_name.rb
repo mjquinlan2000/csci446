046de730bb0a4d585f81c3dab063e784a2ac0284
Mike Quinlan <mquinlan@mines.edu>
2012-02-01 22:09:46 -0700

    adding empty cart feature with tests

---
+++ project03/depot/db/migrate/20120202050748_fix_line_items_column_name.rb
@@ -0,0 +1,9 @@
+class FixLineItemsColumnName < ActiveRecord::Migration
+  def up
+	rename_column :line_items, :product_id, :line_items_product_id
+  end
+
+  def down
+	rename_column :line_items, :line_items_product_id, :product_id
+  end
+end

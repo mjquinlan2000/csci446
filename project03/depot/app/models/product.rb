e07f0e26c0752d9fe2fcceb012287d0fb91157cd
Mike Quinlan <mquinlan@mines.edu>
2012-02-07 00:18:33 -0700

    added last of chapter 12, and generated scaffold for users

--- project03/depot/app/models/product.rb
+++ project03/depot/app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ActiveRecord::Base
   default_scope :order => 'title'
   has_many :line_items, :foreign_key => :line_item_product_id
+  has_many :orders, :through => :line_items
+  
   before_destroy :ensure_not_referenced_by_any_line_items
   
   validates :title, :description, :image_url, :presence => true

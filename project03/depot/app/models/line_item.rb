43cf4f18101bec7a8f02ba5dc5473f547121da56
Mike Quinlan <mquinlan@mines.edu>
2012-02-06 23:47:28 -0700

    changed dependencies as well as yml data

--- project03/depot/app/models/line_item.rb
+++ project03/depot/app/models/line_item.rb
@@ -1,6 +1,7 @@
 class LineItem < ActiveRecord::Base
   belongs_to :product, :foreign_key => :line_item_product_id
   belongs_to :cart
+  belongs_to :order
   
   def total_price
     product.price * quantity

32de80dd5270914d3288936b33613fd32a9422f5
Mike Quinlan <mquinlan@mines.edu>
2012-02-20 19:45:29 -0700

    Redirected to articles path when creating a new article. Also made the colors of the table rows on articles index dynamic so that they are banded :

--- project04/app/controllers/articles_controller.rb
+++ project04/app/controllers/articles_controller.rb
@@ -44,7 +44,7 @@ class ArticlesController < ApplicationController
 
     respond_to do |format|
       if @article.save
-        format.html { redirect_to @article, notice: 'Article was successfully created.' }
+        format.html { redirect_to articles_path, notice: 'Article was successfully created.' }
         format.json { render json: @article, status: :created, location: @article }
       else
         format.html { render action: "new" }

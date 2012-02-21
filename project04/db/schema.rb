8ca152cf9197ced36671aa4f59f5cef9b1a4c8ad
Mike Quinlan <mquinlan@mines.edu>
2012-02-20 23:06:30 -0700

    added migration to add number of updates to articles table

--- project04/db/schema.rb
+++ project04/db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120214002959) do
+ActiveRecord::Schema.define(:version => 20120221060510) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
     t.string   "author"
     t.text     "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
+    t.integer  "updates",     :default => 1
+    t.integer  "num_updates", :default => 0
   end
 
 end

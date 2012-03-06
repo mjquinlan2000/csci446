6633e9058b2c73ee424e6019f28b3bc6be6430bb
Mike Quinlan <mquinlan@mines.edu>
2012-03-05 20:07:25 -0700

    Adding pagination but still can't get numbers to display

--- project05/app/models/article.rb
+++ project05/app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ActiveRecord::Base
   validates :author, :title, :content, :num_updates, :presence => true
   validates :num_updates, :numericality => {:greater_than_or_equal_to => 0 }
   validates :author, :exclusion => { :in => "Pat", :message => "The name Pat is not allowed"} 
+  
+  def self.search(page)
+    paginate :per_page => 10, :page => page, :order => :title
+  end
 end

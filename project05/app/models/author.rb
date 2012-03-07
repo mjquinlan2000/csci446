df7c43420ee294f410315af3d1a9cb383e6670bb
Mike Quinlan <mquinlan@mines.edu>
2012-03-06 17:53:50 -0700

    Adding paperclip stuff to author form partial

--- project05/app/models/author.rb
+++ project05/app/models/author.rb
@@ -1,5 +1,7 @@
 class Author < ActiveRecord::Base
   has_many :articles, :dependent => :delete_all
   
+  has_attached_file :photo
+  
   validates :name, :presence => true, :uniqueness => true, :exclusion => { :in => "Pat", :message => "The name Pat is not allowed" }
 end

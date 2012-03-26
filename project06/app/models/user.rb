c02282168337adf8897d667cff70b13056fefdb2
Mike Quinlan <mquinlan@mines.edu>
2012-03-26 11:34:51 -0600

    Adding some layout stuff and rerouting login/show to login/

--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   validates :first_name, :last_name, :username, :email, :password, :presence => true
   validates :username, :email, :uniqueness => true
+  
+  has_attached_file :photo, :styles => {:small => '150x150'}
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/bmp']
 end
--- project06/config/routes.rb
+++ project06/config/routes.rb
@@ -11,6 +11,8 @@ Gamez::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
+  
+  match 'login' => 'login#show'
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase

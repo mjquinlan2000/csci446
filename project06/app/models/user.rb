f00a9d494d1abc81c53b8cab242714cd34ff74e1
Mike Quinlan <mquinlan@mines.edu>
2012-03-28 12:36:51 -0600

    Adding user seeds as well as email validation

--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   validates :first_name, :last_name, :username, :email, :password, :presence => true
   validates :username, :email, :uniqueness => true
+  validates :email, :format => {:with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/i, :on => :create}
   
   has_attached_file :photo, :styles => {:small => '150x150'}
   validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/bmp']
--- project06/db/seeds.rb
+++ project06/db/seeds.rb
@@ -6,6 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Game.delete_all
+User.delete_all
+
 for i in (1..100)
   Game.create(:title => "Game #{i}", :rating => i%5)
 end
+
+
+for i in (1..10)
+  User.create(:first_name => "test", :last_name => "person", :username => "user#{i}", :email => "email#{i}@somewhere.com", :password => "password")
+end
+
+User.create(:username => "admin", :first_name => "Mike", :last_name => "Quinlan", :password => "password", :email => "loves2splooge@aol.com", :admin => true)
+User.create(:username => "user", :first_name => "Ron", :last_name => "Burgundy", :password => "password", :email => "anchorman@channel4news.com")

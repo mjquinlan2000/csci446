cc72d0296b39dfac5bbf2215895b17d5de3ab73d
Mike Quinlan <mquinlan@mines.edu>
2012-04-03 23:03:34 -0600

    adding views for devise stuff

--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
+  validates :username, :first_name, :last_name, :email, :password, :password_confirmation, :presence => true
+  validates :username, :email, :uniqueness => true
+  
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable, :trackable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :last_name, :first_name
 end

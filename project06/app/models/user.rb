a2f821cb3bb42bdca20ebc1c40401962ff8ae3f0
Mike Quinlan <mquinlan@mines.edu>
2012-04-09 23:30:54 -0600

    Adding users and assignments roles

--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -2,9 +2,10 @@ class User < ActiveRecord::Base
   acts_as_authentic
   
   has_many :games
+  has_many :assignments
   has_many :roles, :through => :assignments
   
   validates :first_name, :username, :last_name, :email, :presence => true
   validates :username, :email, :uniqueness => true
-  validates :username, :length => {:within => 6..15 }
+  validates :username, :length => {:within => 5..15 }
 end
--- project06/db/seeds.rb
+++ project06/db/seeds.rb
@@ -9,13 +9,29 @@
 User.destroy_all
 Game.destroy_all
 
+user_role = Role.find_or_create_by_role("User")
+admin_role = Role.find_or_create_by_role("Admin")
+
 (1..10).each do |i|
-  User.create(:email => "user#{i}@somewhere.com", :first_name => "First", :last_name => "Last", :password => "secret#{i}",
+  user = User.new(:email => "user#{i}@somewhere.com", :first_name => "First", :last_name => "Last", :password => "secret#{i}",
     :password_confirmation => "secret#{i}", :username => "username#{i}")
+  user.roles[0] = user_role
+  if user.save
+    puts "USER CREATED"
+  end
 end
 
 User.all.each do |user|
   (1..3).each do |j|
     Game.create(:title => "Game#{j*j}", :rating => j%5, :user_id => user.id)
   end
+end
+
+admin = User.new(:email => "mjquinlan2000@gmail.com", :first_name => "Michael", :last_name => "Quinlan", :username => "admin",
+  :password => "password", :password_confirmation => "password")
+admin.roles[0] = admin_role
+if admin.save
+  puts "CREATED ADMIN"
+else
+  puts admin
 end

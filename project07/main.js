e83806e65fb3293cdbb35f372867bce9e225f785
Mike Quinlan <mquinlan@mines.edu>
2012-04-23 17:05:16 -0600

    Adding a better way to add click events

--- project07/main.js
+++ project07/main.js
@@ -1,6 +1,6 @@
 var guessesLeft = 10;
 var highScores = new Array([9, "HarryJamesPotter"], [3, "ZedCthulhu"], [2, "NearlyDied"]);
-var secret_number = Math.floor(Math.random()*101);
+var secret_number = Math.floor(Math.random()*100 + 1);
 
 $(function() {
   updateScore(guessesLeft);
@@ -16,3 +16,24 @@ function populateHighScores(scores) {
 function updateScore(score) {
   $('h2#score span#guessesLeft').text(score);
 }
+
+$(document).ready(function(){
+	$('form#guessTheNumber input#btnGuess').click(function(){
+		if($('form#guessTheNumber input#guess').val() != "")
+		{
+			var user_guess = $('form#guessTheNumber input#guess').val();
+			var reg_exp = new RegExp(/^[0-9]*$/);
+			if(!reg_exp.test(user_geuss))
+			{
+				alert("That is not a valid input");
+				return;
+			}else{
+				alert("That didn't work");
+				return;
+			}
+		}else{
+			alert("You must guess something");
+		}
+	});
+});
+
